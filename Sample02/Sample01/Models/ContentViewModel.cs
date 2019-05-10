3b09a6a2657fadaaf827f445c4692fa1c131f374
mark <837216547@qq.com>
2019-05-10 17:07:37 +0800

    新增第三章事例代码

---
+++ Sample02/Sample01/Models/ContentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Models
+{
+    /// <summary>
+    /// 2019.04.02
+    /// xmg
+    /// Content视图模式
+    /// </summary>
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
